576b46f8d027f51b98028fec2f041780bd58f352
CorinneSpruit <corinnemeijdam@hotmail.com>
2020-09-23 21:52:33 +0200

    Tafel van 9

---
+++ src/Tafels.java
@@ -0,0 +1,27 @@
+public class Tafels {
+    public static void main (String [] arg){
+        var x = 9;
+        var y = 1;
+
+        System.out.print(y); System.out.print(" x "); System.out.print(x); System.out.print(" = "); System.out.print(1*x);
+        System.out.println();
+        System.out.print(y+1); System.out.print(" x "); System.out.print(x); System.out.print(" = "); System.out.print(2*x);
+        System.out.println();
+        System.out.print(y+2); System.out.print(" x "); System.out.print(x); System.out.print(" = "); System.out.print(3*x);
+        System.out.println();
+        System.out.print(y+3); System.out.print(" x "); System.out.print(x); System.out.print(" = "); System.out.print(4*x);
+        System.out.println();
+        System.out.print(y+4); System.out.print(" x "); System.out.print(x); System.out.print(" = "); System.out.print(5*x);
+        System.out.println();
+        System.out.print(y+5); System.out.print(" x "); System.out.print(x); System.out.print(" = "); System.out.print(6*x);
+        System.out.println();
+        System.out.print(y+6); System.out.print(" x "); System.out.print(x); System.out.print(" = "); System.out.print(7*x);
+        System.out.println();
+        System.out.print(y+7); System.out.print(" x "); System.out.print(x); System.out.print(" = "); System.out.print(8*x);
+        System.out.println();
+        System.out.print(y+8); System.out.print(" x "); System.out.print(x); System.out.print(" = "); System.out.print(9*x);
+        System.out.println();
+        System.out.print(y+9); System.out.print(" x "); System.out.print(x); System.out.print(" = "); System.out.print(10*x);
+
+    }
+}
